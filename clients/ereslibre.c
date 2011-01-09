b5834bc7450f4cfe45ddd9666d6b4231d5f878ef
Rafael Fernández López <ereslibre@ereslibre.es>
2011-01-09 16:33:06 +0100

    Add animation to the tux

--- clients/ereslibre.c
+++ clients/ereslibre.c
@@ -39,62 +39,89 @@
 struct test {
   struct display *display;
   struct window *window;
-  int x, y, width, height;
+  cairo_surface_t *surface;
+  int width, height;
 };
 
+static int increase = 1;
+static float radius = 0.0;
+
 static void
-draw_rectangle(cairo_surface_t *surface, int width, int height)
+draw_tux(struct test *test)
 {
-  cairo_t *cr = cairo_create(surface);
+  cairo_t *cr = cairo_create(test->surface);
+
+  cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 0.6);
+  cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+  cairo_paint(cr);
+
   cairo_surface_t *tux = cairo_image_surface_create_from_png("./tux.png");
   int w = cairo_image_surface_get_width(tux);
   int h = cairo_image_surface_get_height(tux);
-  
-  cairo_arc(cr, 128.0, 129.0, 76.8, 0, 2 * M_PI);
+
+  cairo_arc(cr, 140.0, 140.0, radius, 0, 2 * M_PI);
   cairo_clip(cr);
   cairo_new_path(cr);
 
-  cairo_scale(cr, 256.0 / w, 256.0 / h);
-  
   cairo_set_source_surface(cr, tux, 0, 50);
   cairo_paint(cr);
   
   cairo_surface_destroy(tux);
 }
 
+static void
+frame_callback(void *data, uint32_t time)
+{
+  struct test *test = data;
+
+  if (radius > 100.0) {
+    increase = 0;
+  } else if (radius < 20.0) {
+    increase = 1;
+  }
+
+  if (increase == 1) {
+    radius += 1.0;
+  } else {
+    radius -= 1.0;
+  }
+
+  draw_tux(test);
+
+  window_damage(test->window, 0, 0, test->width, test->height);
+  wl_display_frame_callback(display_get_display(test->display),
+                            frame_callback, test);
+}
+
 int main(int argc, char *argv[])
 {
-  cairo_surface_t *s;
-	struct test test;
-	struct display *d;
+  struct test test;
+  struct display *d;
+  int size;
 
-	d = display_create(&argc, &argv, NULL);
-	if (d == NULL) {
-		fprintf(stderr, "failed to create display: %m\n");
-		return -1;
-	}
-
-  test.x = 100;
-  test.y = 100;
-	test.width = 256;
-	test.height = 256;
-	test.display = d;
-	test.window = window_create(d, "test", test.width, test.height);
-
-  window_set_decoration(test.window, 0);
-  window_draw(test.window);
-  s = window_get_surface(test.window);
-  if (!s || cairo_surface_status(s) != CAIRO_STATUS_SUCCESS) {
+  d = display_create(&argc, &argv, NULL);
+  if (d == NULL) {
+    fprintf(stderr, "failed to create display: %m\n");
     return -1;
   }
-  
-  draw_rectangle(s, test.width, test.height);
-  cairo_surface_flush(s);
-  cairo_surface_destroy(s);
+
+  test.width = 280;
+  test.height = 280;
+  test.display = d;
+  test.window = window_create(d, "test", test.width, test.height);
+
+  window_set_buffer_type(test.window, WINDOW_BUFFER_TYPE_SHM);
+
+  window_set_decoration(test.window, 0);
+  window_create_surface(test.window);
+  test.surface = window_get_surface(test.window);
+
   window_flush(test.window);
 
   window_set_user_data(test.window, &test);
+  wl_display_frame_callback(display_get_display(d),
+                            frame_callback, &test);
   display_run(d);
   
-	return 0;
+  return 0;
 }
